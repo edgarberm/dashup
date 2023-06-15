96fd6a18620e806d19ffb88f0ae7cf617b0b040d
builtbyedgar <eledgaar@gmail.com>
2023-06-16 01:05:56 +0200

    feat: config

---
+++ .storybook/manager.ts
@@ -0,0 +1,39 @@
+import { addons } from '@storybook/manager-api'
+import { create } from '@storybook/theming/create'
+
+const THEME = create({
+  base: 'dark',
+  brandTitle: 'My custom Storybook',
+  brandUrl: 'https://github.com/builtbyedgar/dashup/',
+  brandImage: process.env.ASSETS_URL + '/Icon-24.png',
+  brandTarget: '_self',
+
+  //
+  // colorPrimary: '#3A10E5',
+  // colorSecondary: '#585C6D',
+
+  // UI
+  // appBg: '#ffffff',
+  // appContentBg: '#ffffff',
+  // appBorderColor: '#585C6D',
+  // appBorderRadius: 2,
+
+  // Text colors
+  // textColor: '#10162F',
+  // textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  // barTextColor: '#9E9E9E',
+  // barSelectedColor: '#585C6D',
+  // barBg: '#ffffff',
+
+  // Form colors
+  // inputBg: '#ffffff',
+  // inputBorder: '#10162F',
+  // inputTextColor: '#10162F',
+  // inputBorderRadius: 2,
+})
+
+addons.setConfig({
+  theme: THEME,
+})
